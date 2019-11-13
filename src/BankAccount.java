f8706b16165b71ebe6049fd7f3ee6cdf0f608153
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-11-13 08:29:14 -0500

    Bank Acount done

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -5,6 +5,37 @@ public class BankAccount {
     private double balance;
     private User accountHolder;
     
+    public BankAccount(int pin, long accountNo, double balance, User accountHolder) {
+        this.pin = pin;
+        this.accountNo = accountNo;
+        this.balance = balance;
+        this.accountHolder = accountHolder;
+    }
+    
+    public int getPin() {
+        return pin;
+    }
+    
+    public long getAccountNo() {
+        return accountNo;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public User getAccountHolder() {
+        return accountHolder;
+    }
+    
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+    
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
+    
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
--- src/User.java
+++ src/User.java
@@ -15,7 +15,6 @@ public class User {
     public String getLastName() {
         return lastName;
     }
-	}
     
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
