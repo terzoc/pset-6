d9acddfb115fa116b678801c77d2eec33008fe2a
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-11-19 09:04:51 -0500

    deposit suports all invalid inputs

--- src/ATM.java
+++ src/ATM.java
@@ -109,8 +109,8 @@ public class ATM {
             if(in.hasNextInt()) {
             	return in.nextInt();
             }else {
-            in.nextLine();
-            return 6;
+            	in.nextLine();
+            	return 6;
             }
         }
         
@@ -119,15 +119,27 @@ public class ATM {
         }
         
         public void deposit() {
-            System.out.print("\nEnter amount: ");
-            double amount = in.nextDouble();
-                
-            int status = activeAccount.deposit(amount);
-            if (status == ATM.INVALID) {
-                System.out.println("\nDeposit rejected. Amount must be greater than $0.00.\n");
-            } else if (status == ATM.SUCCESS) {
-                System.out.println("\nDeposit accepted.\n");
-            }
+        	double amount = 0;
+			boolean validAmount = true;
+    		System.out.print("\nEnter amount: ");
+    		try {
+    			amount = in.nextDouble();
+    		}catch(Exception e) {
+    			validAmount = false;
+    			in.nextLine();
+    		}
+    		
+    		if(validAmount) {
+    			int status = activeAccount.deposit(amount);
+                if (status == ATM.INVALID) {
+                    System.out.println("\nDeposit rejected. Amount must be greater than $0.00.\n");
+                } else if (status == ATM.SUCCESS) {
+                    System.out.println("\nDeposit accepted.\n");
+                }
+    		}else {
+    			System.out.println("\nDeposit rejected. Enter vaild amount.\n");
+    		}
+            
         }
             
         public void withdraw() {
